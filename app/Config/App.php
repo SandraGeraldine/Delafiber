eefb5309bfd4f061f6c86eb93cc2658594b21173
Sandra120704 <sandra@example.com>
2025-11-18 10:46:44 -0500

    configuraciones

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://delafiber.test/';
+    public string $baseURL = 'https://delafiber.com/crm/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'root',
-        'password'     => '',
-        'database'     => 'delafiber',
+        'username'     => 'u647805867_delafibercrm',
+        'password'     => 'Dsandra72',
+        'database'     => 'u647805867_delafibercrm',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
